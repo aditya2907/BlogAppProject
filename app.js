55fd2c7575d53ccf3f1017c6a1a6b4e2e319382b
Aditya Suryawanshi <suryawanshiaditya29@gmail.com>
2021-12-19 10:57:43 +0530

    v1.3
    contact form added

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
-
 
 const postSchema = {
 	title: {
@@ -30,6 +28,27 @@ const postSchema = {
 	}
 }
 const Post = mongoose.model("Post", postSchema);
+
+const contactSchema = {
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	phone: {
+		type: Number,
+		required: true
+	},
+	decription: {
+		type: String,
+		required: true
+	}
+}
+
+const Contact = mongoose.model("Contact", contactSchema);
 
 app.get("/", function (req, res) {
 	Post.find({}, function (err, posts) {
@@ -46,17 +65,29 @@ app.get("/about", function (req, res) {
 	})
 });
 
-app.get("/contact", function (req, res) {
-	res.render("contact", {
-		contactContent: contactContent
-	})
+app.route("/contact")
+.get(function (req, res) {
+	res.render("contact");
+})
+.post(function(req, res){
+	const contact = new Contact({
+		name: req.body.contactName,
+		email: req.body.contactEmail,
+		phone: req.body.contactPhone,
+		decription: req.body.contactDescription
+	});
+	contact.save(function (err) {
+		if (!err) {
+			res.redirect("/");
+		}
+	});
 });
 
-app.get("/compose", function (req, res) {
+app.route("/compose")
+.get(function (req, res) {
 	res.render("compose")
-});
-
-app.post("/compose", function (req, res) {
+})
+.post(function (req, res) {
 	const post = new Post({
 		title: req.body.postTitle,
 		content: req.body.postBody
@@ -68,6 +99,7 @@ app.post("/compose", function (req, res) {
 	});
 });
 
+
 app.get("/posts/:postId", function (req, res) {
 	
 	const requestedPostId = req.params.postId;
@@ -76,10 +108,10 @@ app.get("/posts/:postId", function (req, res) {
 	  res.render("post", {
 		title: posts.title,
 		content: posts.content
-	  });
+		});
 	});
-  
 });
+
 
 app.listen(3000, function () {
 	console.log("Server started on port 3000");
